4d95eb942fc1a7bb9ce0f2c7b25463f9eb530146
Tyler <43707450+changty97@users.noreply.github.com>
2019-07-31 15:46:03 -0700

    First commit

---
+++ game/icehockey.py
@@ -0,0 +1,15 @@
+import pygame 
+pygame.init()
+
+#Setting the window of the game in pixels (ex: 500px by 500px)
+win = pygame.display.set_mode((500, 500))
+
+#Labels your game at the top/tab of the screen
+pygame.display.set_caption("First Game!")
+
+x = 50
+y = 50
+width = 40 
+height = 60
+vel = 5
+
