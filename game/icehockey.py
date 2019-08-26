9c25ee08c58eac4dae44a3e810d2bdcf468fb593
tyler.chang@owp.csus.edu <owp-sp05@csus.edu>
2019-08-26 15:04:30 -0700

    function declaration for background

--- game/icehockey.py
+++ game/icehockey.py
@@ -14,6 +14,12 @@ width = 40
 height = 60
 vel = 25
 
+#Backgound/Window setup
+def redrawGameWindow():
+    win.fill((255, 255, 255))
+    pygame.draw.rect(win, (0, 0, 255), (x , y, width, height))
+    pygame.display.update()
+
 #Main loop 
 run = True
 while run:
@@ -34,8 +40,6 @@ while run:
     if keys[pygame.K_DOWN] and y < screenWidth - width - vel:
         y += vel
 
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (0, 0, 255), (x , y, width, height))
-    pygame.display.update()
+    redrawGameWindow()
 
 pygame.quit()
